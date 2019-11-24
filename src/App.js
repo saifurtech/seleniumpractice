61f372b6ff08a74637c173becc0ac0b483aee1fc
Saifur Rahman <safrrhmn@hotmail.com>
2019-11-24 12:05:42 -0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import {
   Grid,
   makeStyles,
   Paper,
-  ListSubheader
+  ListSubheader,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
 } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,6 +24,18 @@ const useStyles = makeStyles(theme => ({
   },
   control: {
     padding: theme.spacing(2)
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  gridStyle: {
+    backgroundColor: "Lavender"
+  },
+  appBarStyle: {
+    backgroundColor: "Orchid"
   }
 }));
 
@@ -25,31 +43,45 @@ function App() {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      className={(classes.root, "App")}
-      spacing={2}
-      justify="center"
-    >
-      <Grid item xs={3}>
-        <Paper>
-          <ListSubheader>TODO</ListSubheader>
-          <TodoListComponent />
-        </Paper>
+    <>
+      <AppBar position="static" className={classes.appBarStyle}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            My TODO List
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+      <br />
+      <Grid container className={classes.root} spacing={2} justify="center">
+        <Grid item xs={3} className={classes.gridStyle}>
+          <Paper>
+            <ListSubheader>TODO</ListSubheader>
+            <TodoListComponent />
+          </Paper>
+        </Grid>
+        <Grid item xs={3} className={classes.gridStyle}>
+          <Paper>
+            <ListSubheader>IN-PROGRESS</ListSubheader>
+            <TodoListComponent />
+          </Paper>
+        </Grid>
+        <Grid item xs={3} className={classes.gridStyle}>
+          <Paper>
+            <ListSubheader>DONE</ListSubheader>
+            <TodoListComponent />
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={3}>
-        <Paper>
-          <ListSubheader>IN-PROGRESS</ListSubheader>
-          <TodoListComponent />
-        </Paper>
-      </Grid>
-      <Grid item xs={3}>
-        <Paper>
-          <ListSubheader>DONE</ListSubheader>
-          <TodoListComponent />
-        </Paper>
-      </Grid>
-    </Grid>
+    </>
   );
 }
 
